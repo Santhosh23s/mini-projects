12ff6b9d83a4315c9deb952a81262cdc1c13edfa
Santhosh23s <santhoshjeevan88@email.com>
2026-03-12 22:00:35 +0530

    simple calculator app

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public long add(long num1, long num2) {
+        return num1 + num2;
+    }
+
+    public long sub(long num1, long num2) {
+        return num1 - num2;
+    }
+    public long mul(long num1, long num2) {
+        return num1 * num2;
+    }
+    public long div(long num1, long num2) {
+        return num1 / num2;
+    }
+}
